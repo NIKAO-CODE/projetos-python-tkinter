7b71ab1cd9e0d27c42eecf88d5e1b866f5577cb5
NIKAO-CODE <nicolasdscience@gmail.com>
2023-10-20 20:57:34 -0300

    realizei algumas alterações

--- projeto1/projeto1.py
+++ projeto1/projeto1.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+import algoritmo_quicksort
 
 janela = Tk()
 janela.title("Projeto 1")
@@ -14,24 +15,31 @@ label_1 = Label(janela, text="Ordenação", fg="white", bg="black", font=("Audio
 label_1.grid(column=0, row=0)
 
 
-label_2 = Label(janela, text="Insira a sequência para ordenar", fg="black", font=("Audiowide", 15), pady= 9)
+label_2 = Label(janela, text="Insira a sequência para ordenar\nSeparado por espaço",
+                fg="black", font=("Audiowide", 15), pady= 9)
 label_2.grid(column=0, row=1)
 
+
 sequencia = Text(janela, width=50, height=5)
-sequencia.grid(column=0, row=2, pady=40)
+sequencia.grid(column=0, row=2, pady=30)
 
 
 def obter_sequencia():
-    s = sequencia.get("1.0", "end-1c")
+    sequencia_text = sequencia.get("1.0", "end-1c")
+    sequencia_ajustada = sequencia_text.replace(" ", ", ")
+    lista = sequencia_ajustada.split(", ")
 
-    lista = [i for i in s]
     lista.sort()
+    print(lista)
 
-    label_3 = Label(janela, text= lista, fg="black", font=("Audiowide", 15))
+    sequencia_ordenada = ", ".join(lista)
+
+    label_3 = Label(janela, text= sequencia_ordenada, fg="black", font=("Audiowide", 15), relief='solid',
+                    width=36, height=2)
     label_3.grid(column=0, row=3)
 
 
-botao_ordenar = Button(janela, text="Executar", bg="gray", fg="black", width=20, font=15, command=obter_sequencia)
+botao_ordenar = Button(janela, text="Executar", bg="gray", fg="black", width=20, font=15, command=obter_sequencia,)
 botao_ordenar.grid(column=0, row=4)
 
 
